700a42fe755ca46cfc2538ca253b82594580714e
Souheil Inati <souheil.inati@gmail.com>
2019-01-07 06:01:23 -0500

    Add signal statistics.

--- jem/signal_stats.py
+++ jem/signal_stats.py
@@ -1,130 +1,88 @@
-# -*- coding: utf-8 -*-
 import numpy as np
 
-from .logging import logger
 
-# default scale for automatic signal scaling
-SIGNAL_SCALE = 4
+def stats(data, w=None):
+    """Weighted mean and mean and absolute deviation
 
-
-def noise_stats(data, tol=1e-2):
-    """Estimate the statistics of the noise in an image
-
-    Returns the lower fence, median, and upper fence of the trimmed data.
-    The upper fence is a lower bound on a (global) estimate of the noise.
-
-    The algorithm alternates between trimming the data and estimating the quartiles.
-    At each step, the data are trimmed by selecting the voxels with intensity greater
-    than zero and less than the upper fence from the previous step.
-    Iteration stops when the relative change in the UF is less than the specified tolerance.
+    mean = sum(w*data)/sum(w)
+    sigma = sum(w*|data-m|)/sum(w)
     """
-
-    d = data[data > 0].flatten()
-
-    # find the quartiles of the non-zero data
-    q1, q2, q3 = np.percentile(d, [25, 50, 75])
-    logger.debug("Quartiles of the original data: {}, {}, {}".format(q1, q2, q3))
-
-    # find the quartiles of the non-zero data that is less than a cutoff
-    # start with the first quartile and then iterate using the upper fence
-    uf = q1
-
-    # repeat
-    for it in range(20):
-        q1, q2, q3 = np.percentile(d[d <= uf], [25, 50, 75])
-        logger.debug(
-            "Iteration {}. Quartiles of the trimmed data: {}, {}, {}".format(
-                it, q1, q2, q3
-            )
-        )
-        q13 = q3 - q1
-        ufk = q2 + 1.5 * q13
-        # check for convergence
-        if abs(ufk - uf) / uf < tol or ufk < tol:
-            break
-        else:
-            uf = ufk
+    if w is not None:
+        mu = np.sum(w * data) / np.sum(w)
+        sigma = np.sum(w * np.abs(data - mu)) / np.sum(w)
     else:
-        logger.warning("Warning, number of iterations exceeded")
+        mu = np.mean(data)
+        sigma = np.mean(np.abs(data - mu))
 
-    # recompute the quartiles
-    q1, q2, q3 = np.percentile(d[d <= uf], [25, 50, 75])
-    q13 = q3 - q1
-    # q1, q2, q3 describes the noise
-    # anything above this is a noise outlier above (possibly signal)
-    uf = q2 + 1.5 * q13
-    # anything below lf is a signal outlier below (not useful)
-    lf = q2 - 1.5 * q13
-    # but remember that the noise distribution is NOT symmetric, so uf is an underestimate
-
-    return lf, q2, uf
+    return mu, sigma
 
 
-def signal_stats(data):
+def signal_likelihood(data, sigma=1.0):
+    """Return a likelihood that positive valued data is signal
+
+    w(x) = 1 - exp(-d/sigma)
+    """
+    w = 1 - np.exp(-data / sigma)
+    return w
+
+
+def noise_likelihood(data, sigma=1.0):
+    """Return a likelihood that positive valued data is noise
+
+    w(x) = exp(-d/sigma)
+    """
+    w = np.exp(-data / sigma)
+    return w
+
+
+def signal_stats(data, niter=3):
     """Estimate the statistics of the signal in an image
 
-    Estimates the noise level and the mean, standard deviation, median, upper
-    and lower fences of the signal
+    Iterative estimation of signal mean and mad and
+    signal likelihood
     """
 
-    # Trim the noise
-    _, _, uf = noise_stats(data)
-    d = data[data > uf].flatten()
+    # Assume that everything is signal to start with
+    mu, sigma = stats(data)
+    # iterate (two or three iterations is enough)
+    for _iter in range(niter):
+        # Get the likelihood
+        ws = signal_likelihood(data, sigma)
+        # Update
+        mu, sigma = stats(data, ws)
 
-    # compute the mean and standard deviation
-    m = np.mean(d)
-    s = np.std(d)
-
-    # find the quartiles of the signal
-    q1, q2, q3 = np.percentile(d, [25, 50, 75])
-    logger.debug("Quartiles of the signal: {}, {}, {}".format(q1, q2, q3))
-    q13 = q3 - q1
-    # q1, q2, q3 describe the signal, q2 is the median,
-    # anything above uf and below lf are signal outliers
-    uf = q2 + 1.5 * q13
-    lf = q2 - 1.5 * q13
-
-    return m, s, q2, lf, uf
+    return mu, sigma
 
 
-def signal_likelihood(data, uf=None):
-    """Return a likelihood that data is signal
-
-    in SNR units, sigmoid with width 1, shifted to the right by 1
-    ie P(<1)=0, P(2)=0.46, P(3)=0.76, P(4)=0.91, P(5)=0.96
-    """
-    if not uf:
-        _, _, uf = noise_stats(data)
-
-    # The probability that each point has a signal
-    p = (data > uf) * (-1 + 2 / (1 + np.exp(-(data - uf) / uf)))
-
-    return p
+def noise_level(data, sigma_s=1.0):
+    """Estimate the noise level"""
+    # Look at all the non-zero points with noise likelihood greater than 0.5
+    w_n = noise_likelihood(data, sigma_s)
+    noise = data[np.logical_and(data > 0, w_n > 0.5)]
+    mu_n, sigma_n = signal_stats(noise)
+    return sigma_n
 
 
-def auto_scale(data, signal_min=0, signal_max=2 * SIGNAL_SCALE):
-    """
-    Apply a global brightness and contrast adjustment to an image.
+def global_scale(data):
+    """Estimate signal and noise statistics and apply a global scale
 
-    Uses the signal statistics to clip the data between the lower and upper fences and
-    shift and scale to a given range.
-
-    :param data: 2D or 3D image
-    :param signal_min: the minimum value of the output image
-    :param signal_max: the maximum value of the output image
-    :return: clipped, shifted and scaled data
+    :param data: 2D or 3D magnitude image
+    :return: scaled data f
+             signal likelihood w_s
+             noise level sigma_n
     """
 
-    _, _, _, lf, uf = signal_stats(data)
+    # Estimate the signal mean and dispersion
+    mu_s, sigma_s = signal_stats(data)
 
-    # Initialize the output (copy)
-    output = data.astype("float32")
+    # Scale the signal by the dispersion
+    # signal dispersion of f is 1.0
+    f = data / sigma_s
 
-    # Clip
-    output[output < lf] = lf
-    output[output > uf] = uf
+    # Get the signal likelihood
+    w_s = signal_likelihood(f)
 
-    # shift and scale
-    output = (signal_max - signal_min) / (uf - lf) * (output - lf) + signal_min
+    # Get the noise level
+    sigma_n = noise_level(f)
 
-    return output
+    return f, w_s, sigma_n
