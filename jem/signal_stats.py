df3ef307665ea161aad25c70f0d3a5178ddf24e5
Souheil Inati <souheil.inati@gmail.com>
2019-01-17 10:50:31 -0500

    Reverted signal and noise level estimation to soft response

--- jem/signal_stats.py
+++ jem/signal_stats.py
@@ -1,9 +1,6 @@
 import numpy as np
 
-from .logging import logger
-
-NOISE_MAX_ITER = 20
-NOISE_TOL = 1e-2
+SIGNAL_MAX_ITER = 20
 
 
 def stats(data, w=None):
@@ -22,58 +19,6 @@ def stats(data, w=None):
     return mu, sigma
 
 
-def noise_stats(data, niter=NOISE_MAX_ITER, tol=NOISE_TOL):
-    """Estimate the statistics of the noise in an image
-    Returns the lower fence, median, and upper fence of the trimmed data.
-    The upper fence is a lower bound on a (global) estimate of the noise.
-    The algorithm alternates between trimming the data and estimating the quartiles.
-    At each step, the data are trimmed by selecting the voxels with intensity greater
-    than zero and less than the upper fence from the previous step.
-    Iteration stops when the relative change in the UF is less than the specified tolerance
-    or when the maximum number of iterations is reached.
-    """
-
-    d = data[data > 0].flatten()
-
-    # find the quartiles of the non-zero data
-    q1, q2, q3 = np.percentile(d, [25, 50, 75])
-    logger.debug("Quartiles of the original data: {}, {}, {}".format(q1, q2, q3))
-
-    # find the quartiles of the non-zero data that is less than a cutoff
-    # start with the first quartile and then iterate using the upper fence
-    uf = q1
-
-    # repeat
-    for it in range(niter):
-        q1, q2, q3 = np.percentile(d[d <= uf], [25, 50, 75])
-        logger.debug(
-            "Iteration {}. Quartiles of the trimmed data: {}, {}, {}".format(
-                it, q1, q2, q3
-            )
-        )
-        q13 = q3 - q1
-        ufk = q3 + 1.5 * q13
-        # check for convergence
-        if abs(ufk - uf) / uf < tol or ufk < tol:
-            break
-        else:
-            uf = ufk
-    else:
-        logger.warning("Warning, number of iterations exceeded")
-
-    # recompute the quartiles
-    q1, q2, q3 = np.percentile(d[d <= uf], [25, 50, 75])
-    q13 = q3 - q1
-    # q1, q2, q3 describes the noise
-    # anything above this is a noise outlier above (possibly signal)
-    uf = q3 + 1.5 * q13
-    # anything below lf is a signal outlier below (not useful)
-    lf = q1 - 1.5 * q13
-    # but remember that the noise distribution is NOT symmetric, so uf is an underestimate
-
-    return lf, q2, uf
-
-
 def signal_likelihood(data, noise_level):
     """Return a likelihood that data is signal
     in SNR units, sigmoid with width 1, shifted to the right by 1
@@ -87,31 +32,45 @@ def signal_likelihood(data, noise_level):
 
 
 def noise_likelihood(data, noise_level):
-    """Return a likelihood that data is noise.
-    noise_likelihood(x) = 1 - signal_likelihood(x)
+    """Return a likelihood that data is not zero and noise.
+    noise_likelihood(x) = (x>0) * (1 - signal_likelihood(x))
     """
-    p = 1.0 - signal_likelihood(data, noise_level)
+    p = (data > 0) * (1.0 - signal_likelihood(data, noise_level))
 
     return p
 
 
-def signal_stats(data, niter=NOISE_MAX_ITER, tol=NOISE_TOL):
-    """Estimate the statistics of the signal in an image
+def signal_weight(data, sigma=1.0):
+    """Return a weight for (saturating exponential response)
+    w(x) = 1 - exp(-d/sigma)
+    """
+    w = 1.0 - np.exp(-data / sigma)
+    return w
+
+
+def signal_stats(data, niter=3):
+    """Estimate the statistics of the signal and noise in an image
+    Iterative weighted estimation of signal mean and mad
     """
 
-    # Estimate the noise level
-    _, _, uf = noise_stats(data, niter, tol)
+    d = data[data > 0]
 
-    # Compute the signal likelihood
-    w = signal_likelihood(data, uf)
+    # Assume that everything is signal to start with
+    mu_s, sigma_s = stats(d)
+    # iterate (two or three iterations is enough)
+    for _iter in range(niter):
+        # Get the likelihood
+        w_s = signal_weight(d, sigma_s)
+        # Update
+        mu_s, sigma_s = stats(d, w_s)
 
-    # Computed the weighted mean and mad
-    mu, sigma = stats(data, w)
+    w_n = 1.0 - w_s
+    mu_n, sigma_n = stats(d, w_n)
 
-    return uf, mu, sigma, w
+    return mu_s, sigma_s, mu_n, sigma_n
 
 
-def global_scale(data, niter=NOISE_MAX_ITER, tol=NOISE_TOL):
+def global_scale(data, niter=SIGNAL_MAX_ITER):
     """Estimate signal and noise statistics and apply a global scale
 
     :param data: 2D or 3D magnitude image
@@ -120,14 +79,17 @@ def global_scale(data, niter=NOISE_MAX_ITER, tol=NOISE_TOL):
              noise level relative to signal (sigma_n)
     """
 
-    # Estimate the signal properties
-    uf, mu_s, sigma_s, w_s = signal_stats(data, niter, tol)
+    # Estimate the signal statistics
+    mu_s, sigma_s, mu_n, sigma_n = signal_stats(data, niter)
 
     # Scale the signal by the dispersion
     # signal dispersion of f is 1.0
     f = data / sigma_s
 
     # Scale the noise leval
-    sigma_n = uf / sigma_s
+    sigma_n = sigma_n / sigma_s
+
+    # Get a signal likelihood
+    w_s = signal_likelihood(f, sigma_n)
 
     return f, w_s, sigma_n
