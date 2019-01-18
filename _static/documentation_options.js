141884b67a8b658f2612cc0552d525e3f1eebd4a
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-01-18 16:00:29 +0000

    Update docs after building Travis build 62 of InatiLab/jem
    The docs were built from the branch 'master' against the commit
    f898e3ee1802787157418d479f0d044b957fe40c.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/InatiLab/jem/jobs/481422941.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.2.post3+g7065bb9',
+    VERSION: '0.3.2.post4+gf898e3e',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
