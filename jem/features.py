Refactor signal stats to simplify.  Fix laplacian pyramid.  Fix Hessian rotational invariant.

jem/features.py
@@ -54,30 +54,24 @@ def rectify(data, polarity=1):
     return output
 
 
-def local_gain_field(f, w, sigma, scale=NORMALIZATION_SCALE):
+def local_gain_field(f, w=None, sigma=0.001, scale=NORMALIZATION_SCALE):
     """
     Local gain field
     Inverse of the local gain, i.e. multiply by this for local divisive normalization
     """
-    gain = _pinv(weighted_low_pass(f, w, sigma, scale), sigma)
+    if w is not None:
+        gain = _pinv(weighted_low_pass(f, w, sigma, scale), sigma)
+    else:
+        gain = _pinv(low_pass(f, scale), sigma)
 
     return gain
 
 
-def local_scale_normalization(d, w, sigma, scale=NORMALIZATION_SCALE):
+def local_scale_normalization(d, w, sigma=0.001, scale=NORMALIZATION_SCALE):
     """
-    Local normalization by the signal scale.
-    Signal scale is estimated as the mean absolute deviation
+    Local normalization by the signal mean.
     """
-    """
-    Local contrast normalization
-    """
-    if w is not None:
-        dev = weighted_high_pass(d, w, sigma, scale)
-        f = d * local_gain_field(np.abs(dev), w, sigma, scale)
-    else:
-        dev = high_pass(d, scale)
-        f = d * _pinv(low_pass(np.abs(dev), scale), sigma)
+    f = d * local_gain_field(d, w, sigma, scale)
 
     return f
 
@@ -89,10 +83,10 @@ def local_contrast_normalization(d, w=None, sigma=0.001, scale=NORMALIZATION_SCA
     """
     if w is not None:
         dev = weighted_high_pass(d, w, sigma, scale)
-        lc = dev * local_gain_field(np.abs(dev), w, sigma, scale)
     else:
         dev = high_pass(d, scale)
-        lc = dev * _pinv(low_pass(np.abs(dev), scale), sigma)
+
+    lc = dev * local_gain_field(np.abs(dev), w, sigma, scale)
 
     return lc
 
@@ -114,27 +108,21 @@ def laplacian_pyramid(d, w=None, sigma=0.001, num_scales=NUM_SCALES):
     # Initialize the pyramid
     lpyr = []
 
-    # High pass
-    if w is not None:
-        lp = weighted_low_pass(d, w, sigma, scale=0)
-    else:
-        lp = low_pass(d, scale=0)
-
-    # Compute the high pass and append
-    lpyr.append(d - lp)
-
-    # Recursive filtering with gaussian of scale=1
-    # halves the resolution every time
-    for _scale in range(num_scales):
-        # Replace the data with the low pass from the previous scale
-        d = lp.copy()
+    # For each scale blur at this scale and subtract from the previous scale
+    # initialize with the original data
+    previous = d
+    # Start with the scale 0 and go to num_scales
+    # HP=D-G(0), L0=G(0)-G(1), L1=G(1)-G(2), ..., LN=G(N)-G(N+1), LP=G(N+1)
+    for scale in range(num_scales + 1):
         if w is not None:
-            lp = weighted_low_pass(d, w, sigma, scale=1)
+            lp = weighted_low_pass(d, w, sigma, scale=scale)
         else:
-            lp = low_pass(d, scale=1)
+            lp = low_pass(d, scale=scale)
 
-        # Compute this level of the pyramid
-        lpyr.append(d - lp)
+        # Compute this level of the pyramid and append
+        lpyr.append(previous - lp)
+        # Replace the previous
+        previous = lp
 
     # The low pass is left
     lpyr.append(lp)

jem/signal_stats.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-SIGNAL_MAX_ITER = 20
+SIGNAL_MAX_ITER = 5
+SIGMA_SCALE = 0.01
 
 
 def stats(data, w=None):
@@ -19,58 +20,48 @@ def stats(data, w=None):
     return mu, sigma
 
 
-def signal_likelihood(data, noise_level):
-    """Return a likelihood that data is signal
-    in SNR units, sigmoid with width 1, shifted to the right by 1
-    ie P(<1)=0, P(2)=0.46, P(3)=0.76, P(4)=0.91, P(5)=0.96
+def signal_weight(data, mu=1.0):
+    """Return a weight for the signal
+
+    w(x) = 1 - exp(-x/mu)
     """
-    p = (data > noise_level) * (
-        -1.0 + 2.0 / (1.0 + np.exp(-(data - noise_level) / noise_level))
-    )
-
-    return p
-
-
-def noise_likelihood(data, noise_level):
-    """Return a likelihood that data is not zero and noise.
-    noise_likelihood(x) = (x>0) * (1 - signal_likelihood(x))
-    """
-    p = (data > 0) * (1.0 - signal_likelihood(data, noise_level))
-
-    return p
-
-
-def signal_weight(data, sigma=1.0):
-    """Return a weight for (saturating exponential response)
-    w(x) = 1 - exp(-d/sigma)
-    """
-    w = 1.0 - np.exp(-data / sigma)
+    w = 1.0 - np.exp(-data / mu)
     return w
 
 
-def signal_stats(data, niter=3):
-    """Estimate the statistics of the signal and noise in an image
-    Iterative weighted estimation of signal mean and mad
+def noise_weight(data, mu=1.0):
+    """Return a weight for the noise
+
+    w(x) = (x>0) * exp(-x/mu)
+    """
+    w = (data > 0) * np.exp(-data / mu)
+    return w
+
+
+def signal_stats(data, niter=SIGNAL_MAX_ITER):
+    """Iterative weighted estimation of signal mean and signal lower bound
     """
 
     d = data[data > 0]
 
     # Assume that everything is signal to start with
-    mu_s, sigma_s = stats(d)
+    mu_s = np.mean(d)
+
     # iterate (two or three iterations is enough)
     for _iter in range(niter):
         # Get the likelihood
-        w_s = signal_weight(d, sigma_s)
+        w = signal_weight(d, mu_s)
         # Update
-        mu_s, sigma_s = stats(d, w_s)
+        mu_s = np.sum(w * d) / np.sum(w)
 
-    w_n = 1.0 - w_s
-    mu_n, sigma_n = stats(d, w_n)
+    # Estimate a lower bound on the signal
+    w_n = noise_weight(d, mu_s)
+    mu_n = np.sum(d * w_n) / np.sum(w_n)
 
-    return mu_s, sigma_s, mu_n, sigma_n
+    return mu_s, mu_n
 
 
-def global_scale(data, niter=SIGNAL_MAX_ITER):
+def global_scale(data, sigma_scale=SIGMA_SCALE, niter=SIGNAL_MAX_ITER):
     """Estimate signal and noise statistics and apply a global scale
 
     :param data: 2D or 3D magnitude image
@@ -79,17 +70,16 @@ def global_scale(data, niter=SIGNAL_MAX_ITER):
              noise level relative to signal (sigma_n)
     """
 
-    # Estimate the signal statistics
-    mu_s, sigma_s, mu_n, sigma_n = signal_stats(data, niter)
+    # Estimate the signal mean and signal lower bound
+    mu_s, mu_n = signal_stats(data, niter)
 
-    # Scale the signal by the dispersion
-    # signal dispersion of f is 1.0
-    f = data / sigma_s
+    # Compute a signal weight based on the signal lower bound
+    w = signal_weight(data, mu_n)
 
-    # Scale the noise leval
-    sigma_n = sigma_n / sigma_s
+    # Scale the signal by the signal mean
+    f = data / mu_s
 
-    # Get a signal likelihood
-    w_s = signal_likelihood(f, sigma_n)
+    # Set the reguralization factor to 0.01*scaled signal lower bound
+    sigma = sigma_scale * mu_n / mu_s
 
-    return f, w_s, sigma_n
+    return f, w, sigma