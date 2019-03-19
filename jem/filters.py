fixed gradient function in filters.py - fixed bug with dz in 3D

jem/filters.py
@@ -217,7 +217,7 @@ def gradient(data, scale=1):
         if np.isrealobj(data):
             dx = np.real(dx)
             dy = np.real(dy)
-            dz = np.real(dy)
+            dz = np.real(dz)
         return [dx, dy, dz]
     else:
         raise RuntimeError(