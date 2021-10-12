90c6c97e073ec3ba4b1eb1ea16f4b4ebb831a592
ShervinAbd92 <ShervinAbd92@users.noreply.github.com>
2021-10-12 13:56:07 +0000

    Deploying to gh-pages from @ InatiLab/jem@21fcafdd5d9591ff0c0ad3b32617486433cf2708 🚀

--- build/html/_static/documentation_options.js
+++ build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post1+g67e2256',
+    VERSION: '0.post1+g21fcafd',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
